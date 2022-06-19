c29f14955bf5a520d6992807079f179244085d06
Agneeshz <84716985+Agneeshz@users.noreply.github.com>
2022-06-19 18:07:58 +0530

    frontend setup

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,115 @@
+import { useState, useEffect } from "react";
+import PrimaryButton from "../components/primary-button";
+import abi from "../utils/Keyboards.json"
+import { ethers } from "ethers";
+
 export default function Home() {
-  return <p>The start of our app!</p>
+  const [ethereum, setEthereum] = useState(undefined);
+  const [connectedAccount, setConnectedAccount] = useState(undefined);
+  const [keyboards, setKeyboards] = useState([])
+  const [newKeyboard, setNewKeyboard] = useState("") // this is new!  
+  
+  const contractAddress = '0x3211915B1E6aD68DCb358f2247b675f94f382e03';
+  const contractABI = abi.abi;
+  
+
+  const handleAccounts = (accounts) => {
+    if (accounts.length > 0) {
+      const account = accounts[0];
+      console.log('We have an authorized account: ', account);
+      setConnectedAccount(account);
+    } else {
+      console.log("No authorized accounts yet")
+    }
+  };
+  
+  const getConnectedAccount = async () => {
+    if (window.ethereum) {
+      setEthereum(window.ethereum);
+    }
+  
+    if (ethereum) {
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+      handleAccounts(accounts);
+    }
+  };
+  useEffect(() => getConnectedAccount(), []);
+  
+  const connectAccount = async () => {
+    if (!ethereum) {
+      alert('MetaMask is required to connect an account');
+      return;
+    }
+  
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    handleAccounts(accounts);
+  };
+
+  const getKeyboards = async () => {
+    if (ethereum && connectedAccount) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const keyboardsContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+      const keyboards = await keyboardsContract.getKeyboards();
+      console.log('Retrieved keyboards...', keyboards)
+      setKeyboards(keyboards)
+    }
+  }
+  useEffect(() => getKeyboards(), [connectedAccount])
+
+  const submitCreate = async (e) => {
+    e.preventDefault();
+
+    if (!ethereum) {
+      console.error('Ethereum object is required to create a keyboard');
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const keyboardsContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    const createTxn = await keyboardsContract.create(newKeyboard);
+    console.log('Create transaction started...', createTxn.hash);
+
+    await createTxn.wait();
+    console.log('Created keyboard!', createTxn.hash);
+
+    await getKeyboards();
+  }
+
+  
+
+  if (!ethereum) {
+    return <p>Please install MetaMask to connect to this site</p>
+  }
+  
+    if (!connectedAccount) {
+      return <PrimaryButton onClick={connectAccount}>Connect MetaMask Wallet</PrimaryButton>
+    }
+  
+    if (keyboards.length > 0) {
+      return (
+        <div className="flex flex-col gap-4">
+          <PrimaryButton type="link" href="/create">Create a Keyboard!</PrimaryButton>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2 p-2">
+            {keyboards.map(
+              ([kind, isPBT, filter], i) => (
+                <Keyboard key={i} kind={kind} isPBT={isPBT} filter={filter} />
+              )
+            )}
+          </div>
+        </div>
+      )
+    }
+  
+    // No keyboards yet
+    return (
+      <div className="flex flex-col gap-4">
+        <PrimaryButton type="link" href="/create">Create a Keyboard!</PrimaryButton>
+        <p>No keyboards yet!</p>
+      </div>
+    )
+    
 }
--- scripts/start.js
+++ scripts/start.js
@@ -1,22 +1,38 @@
+// async function main() {
+//     const [owner, somebodyElse] = await hre.ethers.getSigners();
+//     const keyboardsContractFactory = await hre.ethers.getContractFactory("Keyboards");
+//     const keyboardsContract = await keyboardsContractFactory.deploy();
+//     await keyboardsContract.deployed();
+  
+//     const keyboardTxn1 = await keyboardsContract.create("A really great keyboard!");
+//     await keyboardTxn1.wait();
+  
+//     const keyboardTx2 = await keyboardsContract.connect(somebodyElse).create("An even better keyboard!");
+//     await keyboardTx2.wait();
+  
+//     keyboards = await keyboardsContract.getKeyboards();
+//     console.log("We got the keyboards!", keyboards);
+  
+//     keyboards = await keyboardsContract.connect(somebodyElse).getKeyboards();
+//     console.log("And as somebody else!", keyboards);
+//   }
+
 async function main() {
-    const [owner, somebodyElse] = await hre.ethers.getSigners();
-    const keyboardsContractFactory = await hre.ethers.getContractFactory("Keyboards");
-    const keyboardsContract = await keyboardsContractFactory.deploy();
-    await keyboardsContract.deployed();
-  
-    const keyboardTxn1 = await keyboardsContract.create("A really great keyboard!");
-    await keyboardTxn1.wait();
-  
-    const keyboardTx2 = await keyboardsContract.connect(somebodyElse).create("An even better keyboard!");
-    await keyboardTx2.wait();
-  
-    keyboards = await keyboardsContract.getKeyboards();
-    console.log("We got the keyboards!", keyboards);
-  
-    keyboards = await keyboardsContract.connect(somebodyElse).getKeyboards();
-    console.log("And as somebody else!", keyboards);
-  }
-  
+  const keyboardsContractFactory = await hre.ethers.getContractFactory("Keyboards");
+  const keyboardsContract = await keyboardsContractFactory.deploy();
+  await keyboardsContract.deployed();
+
+  const keyboardTxn1 = await keyboardsContract.create(0, true, "sepia");
+  await keyboardTxn1.wait();
+
+  const keyboardTxn2 = await keyboardsContract.create(1, false, "grayscale");
+  await keyboardTxn2.wait();
+
+  keyboards = await keyboardsContract.getKeyboards();
+  console.log("We got the keyboards!", keyboards);
+}
+
+
   // We recommend this pattern to be able to use async/await everywhere
   // and properly handle errors.
   main()
